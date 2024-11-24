7cb6807271eaeb9cf29cc5019766f3f45feadd2d
Aimielab <adjetey35@hotmail.fr>
2024-11-24 14:23:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,8 +19,8 @@ SILICON_PIC = os.environ.get("SILICON_PIC", "https://envs.sh/KGT.jpg")
 API_ID = int(getenv("API_ID", "20763817"))
 API_HASH = str(getenv("API_HASH", "07186e8f2ffe607e99eedf7eaa5e630b"))
 BOT_TOKEN = str(getenv("BOT_TOKEN", ""))
-FORCE_SUB = os.environ.get("FORCE_SUB", "moviesandwebserieshubOrzz") 
-MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://Hop:Cub@cluster0.5d3ch.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",))
+FORCE_SUB = os.environ.get("FORCE_SUB", "moviesandwebserieshubOrzz")
+MONGO_DB = str(getenv("MONGO_DB", "mongodb+srv://Hop:Cub@cluster0.5d3ch.mongodb.net/?retryWrites=true&w=majority",))
 DEF_CAP = str(
     getenv(
         "DEF_CAP",
